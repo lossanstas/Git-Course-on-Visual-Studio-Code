3ab6c8f9a7e67645940c53f5ad93182b52b3e1bd
lossanstas <lossanstas@yandex.ru>
2024-04-17 19:24:49 +0300

    added recursive function in to file 1_23_Func_Pyth_Agabekyav.py

--- Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
+++ Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
@@ -7,15 +7,33 @@
 # message = say_goodbye
 # message()
 
-def sum(a, b):
-    return a + b
-def mult(a, b):
-    return a * b
-operation = sum
-result = operation(5, 6)
-print(result)
+# def sum(a, b):
+#     return a + b
+# def mult(a, b):
+#     return a * b
+# operation = sum
+# result = operation(5, 6)
+# print(result)
 
-operation = mult
-print(operation(5,6))
+# operation = mult
+# print(operation(5,6))
 
-25:00
+# Рекурсивная функция.
+# def rec_func(x):
+#     if x < 4:
+#         print(x)
+#         rec_func(x+1)
+#         print(x)
+# rec_func(1)
+
+# Рекурсивная функция: фактериал. (4*3*2*1)
+def facterial(x):
+    if x == 1:
+        return 1
+    return facterial(x-1)*x
+print(facterial(4))
+
+45:00
+
+
+
