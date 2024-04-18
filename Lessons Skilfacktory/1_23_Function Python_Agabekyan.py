bb04bdf24153ed56b9e2598fd207ddb7d6f2523d
lossanstas <lossanstas@yandex.ru>
2024-04-18 16:55:45 +0300

    file 1_23_Func_Pyth_Agabekyan.py

--- Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
+++ Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
@@ -26,13 +26,52 @@
 #         print(x)
 # rec_func(1)
 
-# Рекурсивная функция: фактериал. (4*3*2*1)
-def facterial(x):
-    if x == 1:
-        return 1
-    return facterial(x-1)*x
-print(facterial(4))
+# # Рекурсивная функция: фактериал. (4*3*2*1)
+# def facterial(x):
+#     if x == 1:
+#         return 1
+#     return facterial(x-1)*x
+# print(facterial(4))
 
+# ДЕКОРАТОР - функция дли добовления дополнительных возможностей другой функции без изменения её ссодержимого.
+# Способы декорирования: способ №1:
+# def my_decor(func):
+#     def wrapper():
+#         print('start')
+#         func()
+#         print('end')
+#     return wrapper
+# def my_func():
+#     print('Тут основная функция')
+# my = my_decor(my_func)
+# my()
+
+# Способ №2 (Более распространённый и удобный). С помощью @. Нужно будет написать имя декоратора. 
+# На базе кода из варианта №1.
+# def my_decor(func):
+#     def wrapper():
+#         print('start')
+#         func()
+#         print('end')
+#     return wrapper
+# @my_decor     # Первое изменение. Название декоратора.
+# def my_func():
+#     print('Тут основная функция')
+# # my = my_decor(my_func)   # Эти строки можно удалить
+# # my()                     # Эти строки можно удалить
+# my_func()
+
+# Как быть, если функция должна получить данные и их обработать? (на базе кода №2)
+def my_decor(func):
+    def wrapper():
+        print('start')
+        func()
+        print('end')
+    return wrapper
+@my_decor     # Первое изменение. Название декоратора.
+def my_func():
+    print('Тут основная функция')
+my_func()
 
 
 
