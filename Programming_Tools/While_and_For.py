9251fa96e7508fe118e0565b2cd67bee45189ebf
lossanstas <lossanstas@yandex.ru>
2024-04-20 17:48:32 +0300

    Args_Kwargs.py

---
+++ Programming_Tools/While_and_For.py
@@ -0,0 +1,27 @@
+# # Нарезка
+# name = "snow storm"
+# print(name[6:8])  # "to"
+
+# # Целочисленное деление
+# x = 4.5
+# y = 2
+# print(x // y)   # 2.0
+
+# # Применение ключевого значения "elif"
+# x = True
+# y = False
+# z = False
+# if not x or y:
+#     print(1)
+# elif not x or not y and z:
+#     print(2)
+# elif not x or y or not y and x:
+#     print(3)
+# else:
+#     print(4)
+
+
+# x = 23
+# num = 0
+# if x > 10 or 11:
+#     print(num)
