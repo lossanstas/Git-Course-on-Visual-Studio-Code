19280ef46a04b3e44b3c043168c8f69d067848ae
lossanstas <lossanstas@yandex.ru>
2024-04-22 11:56:41 +0300

    created file Cicles for in range.py

---
+++ Lessons Skilfacktory/6_25_Basic_Python_Agabekyan.py
@@ -0,0 +1,12 @@
+# data = [1, 2, 3, 4, 5, 6, 7, 8]
+# for item in data:
+#     if item % 2 == 0:
+#         print(item)
+
+# a = 2345
+# print(a % 10)   # 5
+
+# data = {1: 'hello', 2: 'good', 3: 'bye'}
+# for i in data.keys(): # data. >>>  после точки выходят подсказки: keys; value; и т.д.
+#     print(i)
+
