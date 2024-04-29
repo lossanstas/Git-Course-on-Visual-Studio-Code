d8de6babb9fcc3c849714b4a6366eb6a4f2b675b
lossanstas <lossanstas@yandex.ru>
2024-04-29 14:00:00 +0300

    webilar cicles

--- Programming_Tools/Numbers.py
+++ Programming_Tools/Numbers.py
@@ -1,9 +1,9 @@
-def print_2_add_2():
-    result = 2 + 2
-    print(result)
-print_2_add_2()
+# def print_2_add_2():
+#     result = 2 + 2
+#     print(result)
+# print_2_add_2()
 
-def print_hello():
-    result = "Hello, World!"
-    print(result)
-print_hello()
+# def print_hello():
+#     result = "Hello, World!"
+#     print(result)
+# print_hello()
