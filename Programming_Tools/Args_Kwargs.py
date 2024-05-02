8b1cd92dd1692ff376cd1438995fa116895559d9
lossanstas <lossanstas@yandex.ru>
2024-05-02 12:48:28 +0300

    Delete file Function args and kwargs.py

--- Programming_Tools/Args_Kwargs.py
+++ Programming_Tools/Args_Kwargs.py
@@ -1,3 +1,10 @@
 # numbers = [2, 1, 3, 4, 7]
 # more_numbers = [*numbers, 11, 18]
 # print(*more_numbers, sep='; ')
+
+def my_sum(*numbers):
+    result = 0
+    for i in numbers:
+        result += i
+    print(f'sum: {result}')
+my_sum(10, 10, 8, 8, 6, 6, 4, 4, 2, 2)
