122363c2e04e9d800b785e4a0873ef5c44fd12f6
lossanstas <lossanstas@yandex.ru>
2024-05-10 12:32:27 +0300

    vebinar

--- Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
+++ Lessons Skilfacktory/1_23_Function Python_Agabekyan.py
@@ -102,4 +102,102 @@
 # print(c)
 
 
-# 15_26 Вэбинар 13.12.2023 21:44
+# 15_26 Вэбинар 13.12.2023 21:44 codewars.com задачи и примеры
+#staring.split(separator*, maxsplit*)
+# my_str = ('Программирование в Pyton')
+# print(my_str.split())
+
+# def multiply_nums(numbers):
+#     p = 1
+#     for x in numbers.split(','):
+#         p *= int(x)
+#     return p
+# print(multiply_nums('2, 3'))
+
+# Задача. Вернуть таблицу умножения для числа, которое всегда является целым числом от 1 до 10, напр. для числа 5.
+# 1 * 5 = 5
+# 2 * 5 = 10 и т.д.
+
+# def multy_table(number):
+#     st = str()
+#     for x in range(1, 11):
+#         z = number * x
+#         st += f'{x} * {number} = {z}\n'
+#     return st
+# print(multy_table(5)) 
+
+
+# a = '2,3' # Не использовать пробел. Иначе 3 сдвинется в нижней строке.
+# b = a.split(',')
+# for i in b:
+#     print(i)
+
+# Задача. Изограмма - это слово, где нет повторяющихся или следующих по порядку букв. 
+#Опредилить, является ли слово изограммой или нет. aba==False  abs==True
+
+# def is_isogram(string):              # Шаг №1. Опредилить функцию.
+#     string = string.lower()          # Шаг №4а Перевод букв в прописной (нижний) регистр. Встр. метод .lower()
+#     for item in string:              # Шаг №3. Создание цикла.В данном случае для значений ("item") в стринг.
+#         if string.count(item) > 1:   # Шаг №4. У строки есть встроенный метод .count(), 
+#             return False             # который отвечает за количество элементов.
+#         return True
+# print(is_isogram('Abs'))              # Шаг №2. Опредилить конечную задачу функции (разбираемое слово)
+
+# Задача. Найти наименьшее число из списка.
+# def find_smallest_int(arr):
+#     max = arr[0]
+#     for i in range(0, len(arr)):
+#         if arr[i] < max:
+#             max = arr[i]
+#     return max
+# print(find_smallest_int([2, 345, 96, 75, 1]))
+
+# name = 'Tom'
+# def say_hi():               # Глобальная переменные.
+#     print('Hello', name)
+# def say_bye():
+#     print('Good bye', name)
+# say_hi()
+# say_bye()
+
+# def say_hi():               # Локальная переменные.
+#     name = 'Tom'
+#     surname = 'Johnson'
+#     print('Hello', name, surname)
+
+# def say_bye():
+#     name = 'Cventin'        # Локальная переменные.
+#     surname = 'Tarantino'
+#     print('Good bye', name, surname)
+# say_hi()
+# say_bye()
+
+# name = 'Tom'
+# def say_hi():               # Глобальная переменные.
+#     global name             # использование встроенного метода global
+#     Surname = 'Johns'
+#     name = f'{name}, Bilovich'
+#     print(name)
+#     print(Surname)
+# say_hi()
+
+# # nonlocal использование нелокальных переменных
+# def outer():
+#     x = 'Локальная переменная'
+#     def inner():
+#         nonlocal x
+#         print('Вложенная функция', x)
+#     inner()
+#     print('Внешняя функция', x)
+# outer()
+
+# x = 5           # Объединение глобальных и локальных переменных.
+# def foo():
+#     x = 10
+#     print('local "X":', x)
+# foo()
+# print('global "X":', x)
+
+
+
+# 46:00
--- Programming_Tools/Args_Kwargs.py
+++ Programming_Tools/Args_Kwargs.py
@@ -1,10 +1,34 @@
+# *args/**kwargs (Вместо "args" или "kwargs" можно использовать любые буквосочетания или слова)
+# *args распаковка по позиции (списки, строки)
+# **kwargs распаковка по имени (только словари)
+
+# def fun(*abc):
+#     print(abc[2])
+# fun('Pithon', 'C++', 'Jumla')
+
+def func(*args):
+    result = 0
+    for item in args:
+        result += item
+    return result
+num = (1, 2, 3, 4, 5)
+print(func(*num))
+
+
+# def fun(**dict):
+#     print(dict)   #  Запаковка в словарь
+# fun(name='Tom', surname='Cruse')
+
+
+
+
 # numbers = [2, 1, 3, 4, 7]
 # more_numbers = [*numbers, 11, 18]
 # print(*more_numbers, sep='; ')
 
-def my_sum(*numbers):
-    result = 0
-    for i in numbers:
-        result += i
-    print(f'sum: {result}')
-my_sum(10, 10, 8, 8, 6, 6, 4, 4, 2, 2)
+# def my_sum(*numbers):
+#     result = 0
+#     for i in numbers:
+#         result += i
+#     print(f'sum: {result}')
+# my_sum(10, 10, 8, 8, 6, 6, 4, 4, 2, 2)
